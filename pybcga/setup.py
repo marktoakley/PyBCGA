3c052b168963577fadbcf58b678471922a011adb
mark <marktoakley@hotmail.com>
2014-09-22 13:39:32 +0100

    Added setup script.

---
+++ pybcga/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='PyBCGA',
+    version='0',
+    author='Mark Oakley',
+    author_email='marktoakley@hotmail.com',
+    packages=['bcga'],
+    scripts=['bcga/run_bcga.py']
+)
