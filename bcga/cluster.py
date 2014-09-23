4f0fafce5e603e5d83f44e1cbc8b157cb4b94d3f
mark <marktoakley@hotmail.com>
2014-09-23 12:46:49 +0100

    Cluster coordinates now stored as a 2-D array. This is converted to a 1-D array for the pele interface.

--- bcga/cluster.py
+++ bcga/cluster.py
@@ -13,10 +13,9 @@ class Cluster:
     def __init__(self,natoms):
         '''Generate a random cluster'''
         self.natoms=natoms
-        self.coords = np.random.uniform(-1, 1, [3*natoms]) * 0.7 * float(natoms)**(1./3)
-#        self.coords=(np.random.rand(natoms,3) -0.5) * 1.4 * float(natoms)
+#        self.coords = np.random.uniform(-1, 1, [3*natoms]) * 0.7 * float(natoms)**(1./3)
+        self.coords=(np.random.rand(natoms,3) -0.5) * 1.4 * float(natoms)
         self.quenched=False
-
         
     def get_energy(self):
         """Energy of minimised cluster"""
@@ -32,30 +31,29 @@ class Cluster:
         '''Minimise a Lennard-Jones cluster with the LBFGS minimiser.
         Eventually, potential should be user-defined.'''
         potential = lj.LJ()
-        quench = lambda coords : mylbfgs(self.coords, potential)
-        res = quench(self.coords)
+        quench = lambda coords : mylbfgs(self.coords.flatten(), potential)
+        res = quench(self.coords.flatten())
         self.energy = res.energy
-        self.coords=res.coords
+        self.coords=np.reshape(res.coords,(-1,3))
         
     def z_sort(self):
         '''Re-orders the atoms in a cluster along the z-axis'''
-        #print(self.coords)
-        b=np.reshape(self.coords,(-1,3))
-        c=b[np.lexsort(b.T)]
-        self.coords=c.flatten()
-        #print(self.coords)
+        self.coords=self.coords[np.lexsort(self.coords.T)]
+        self.print_coords()
         
     def print_coords(self):
         '''Print coordinates of cluster to std out'''
         for i in range(0,self.natoms):
-            print(str(i+1)+"\t"+str(self.coords[3*i:3*i+3]))
+            print(str(i+1)+"\t"+str(self.coords[i]))
             
     def write_xyz(self,xyz_file):
         '''Write the cluster's coordinates to an xyz file.'''
         xyz_file.write(str(self.natoms)+"\n")
         xyz_file.write("Energy: "+str(self.get_energy())+"\n")
         for i in range(0,self.natoms):
-            xyz_file.write("X "+str(self.coords[3*i])+
-                       " "+str(self.coords[3*i+1])+
-                       " "+str(self.coords[3*i+2])+"\n")
+            xyz_file.write("X "+str(self.coords[i,0])+
+                       " "+str(self.coords[i,1])+
+                       " "+str(self.coords[i,2])+"\n")
+
+        
         
