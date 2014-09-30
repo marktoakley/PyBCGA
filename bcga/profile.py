84ed1b19e36d410c3f17c7685648630de4de675d
mark <marktoakley@hotmail.com>
2014-09-30 21:05:53 +0100

    Updated profiler to deal with new arguments for GeneticAlgorithm constructor.

--- bcga/profile.py
+++ bcga/profile.py
@@ -3,5 +3,10 @@
 '''
 import cProfile
 from bcga.genetic_algorithm import Genetic_algorithm as ga
+from pele.systems.ljcluster import LJCluster
+from bcga.cluster_factory import ClusterFactory
 
-cProfile.run('ga(13).run()')
+natoms=13
+system = LJCluster(13)
+factory=ClusterFactory(13,system)
+cProfile.run('ga(13,factory).run()')
